1261a90dcf84cf5a0a9c1d401ae6dd1ed3e930c7
Klepsiphron <klepsiphron@users.noreply.github.com>
2026-06-10 04:13:04 -0400

    chore: auto-format and lint fixes

--- packages/sdk-python/src/agenttrace/core.py
+++ packages/sdk-python/src/agenttrace/core.py
@@ -780,7 +780,7 @@ class AgentUsageTracker:
 
 # ---- Singleton / module level API ----
 
-VERSION = "0.4.20"
+VERSION = "0.4.21"
 PACKAGE_NAME = "agenttrace-io"
 
 _instance: Optional[AgentTrace] = None
